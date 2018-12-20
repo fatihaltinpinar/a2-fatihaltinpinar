1efb91ca48f76e5a687b7b4a091426b8b9e525a9
faati <fatihaltinpinar@gmail.com>
2018-12-20 13:48:58 +0300

    Revert "Printing some values for debugging" since saving to a file did not solved my problem with heroku's memory management.

--- bottle_app.py
+++ bottle_app.py
@@ -8,19 +8,11 @@ from bottle import error, route, run, default_app, debug, static_file
 from bottle import request, redirect
 from songs import songs
 from datetime import datetime
-import json
 
 import hash_password # This code has taken from link below.
 # https://bitbucket.org/damienjadeduff/hashing_example/raw/master/hash_password.py
 
-with open('comments') as file:
-        comments = json.load(file)
-
-
-def add_comment(comment):
-        comments.insert(0, comment)
-        with open('comments', 'w') as file:
-            json.dump(comments, file)
+comments = []
 
 
 @route('/submit', method="POST")
@@ -30,28 +22,23 @@ def submit():
     if hash_password.create_hash(password) == 'b493d48364afe44d11c0165cf470a4164d1e2609911ef998be868d46ade3de4e':
 
         # comment_text = request.forms.get('commentText')  returns byte string
-
         # Use this code instead of one above since it returns utf-8
         # encoded string rather that byte string like the code above does
-        comment_text = request.forms.commentText  # returns utf-8 encoded string
+        comment_text = request.forms.commentText # return utf-8 encoded string
 
-# print('comment text = ', comment_text)
+        print('comment text = ' , comment_text)
 
         if comment_text != '':
-            time = datetime.now().strftime('%d %b %y at %H:%M:%S')
-            comment = {'commentText': comment_text, 'time': time}
-# print('username = ', request.forms.username, '\n'*10)
+
+            comment = {'commentText': comment_text, 'time': datetime.now()}
+
             if request.forms.anonymous == 'yes' or request.forms.username == '':
                 comment['username'] = 'Anonymous'
             else:
                 comment['username'] = request.forms.username
                 # this code returns byte string so it is disabled
                 # comment['username'] = request.forms.get('username')
-
-            add_comment(comment)
-             # comments.insert(0, comment)
-
-
+            comments.insert(0, comment)
     redirect('/songs.html')
 
 
